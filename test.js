bb31e8000893ce3c5bd30edec167a1c8b8f9ace0
Sany <44934721+often@users.noreply.github.com>
2020-06-17 11:17:11 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-import cstring from './mod.js'
+import cstring from 'https://deno.land/x/cstring/mod.js'
 
 console.log(cstring(12)) // returns a cryptographically random string in length of 12 characters
